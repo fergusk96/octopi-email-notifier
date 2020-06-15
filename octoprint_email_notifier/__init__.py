265f7d1942c5146368dcdd16ea90a0ade039a367
fergusk <fergusk@qualtrics.com>
2020-06-15 20:41:55 +0100

    first commit

---
+++ octoprint_email_notifier/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import octoprint.plugin
+
+class EmailNotifierPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
+
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = EmailNotifierPlugin	()
